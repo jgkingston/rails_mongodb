15fe433788f365d68d1e6e7ac88c142164c015ca
Jamie Kingston <jamie.kingston@gmail.com>
2014-10-08 22:42:50 -0400

    Add before actions to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :cors_set_access_control_headers
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :github_username )}
@@ -14,4 +16,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for resource
     root_path
   end
+
+  private
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
 end
